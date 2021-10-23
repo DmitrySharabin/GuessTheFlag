edc7869321e863549fa665b27a474f8dc90788e6
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-10-23 18:46:34 +0300

    Day 20 — Buttons and images

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,22 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+//        Button("Delete selection") {
+//            print("Now deleting...")
+//        }
         
-//        LinearGradient(gradient: Gradient(stops: [
-//            Gradient.Stop(color: .white, location: 0.45),
-//            Gradient.Stop(color: .black, location: 0.55)
-//        ]), startPoint: .top, endPoint: .bottom)
+//        Button("Delete selection", action: executeDelete)
         
-//        LinearGradient(gradient: Gradient(stops: [
-//            .init(color: .white, location: 0.45),
-//            .init(color: .black, location: 0.55)
-//        ]), startPoint: .top, endPoint: .bottom)
+//        Button("Delete selection", role: .destructive, action: executeDelete)
         
-//        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+//        VStack {
+//            Button("Button 1") { }
+//            .buttonStyle(.bordered)
+//
+//            Button("Button 2", role: .destructive) { }
+//            .buttonStyle(.bordered)
+//
+//            Button("Button 3") { }
+//            .buttonStyle(.borderedProminent)
+//            .tint(.mint)
+//
+//            Button("Button 4", role: .destructive) { }
+//            .buttonStyle(.borderedProminent)
+//        }
         
-        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+//        Button {
+//            print("Button was tapped!")
+//        } label: {
+//            Text("Tap me!")
+//                .padding()
+//                .foregroundColor(.white)
+//                .background(.red)
+//        }
+        
+//        Image(systemName: "pencil")
+        
+//        Button {
+//            print("Button was tapped!")
+//        } label: {
+//            Image(systemName: "pencil")
+//        }
+        
+        Button {
+            print("Button was tapped!")
+        } label: {
+            Label("Edit", systemImage: "pencil")
+        }
+//        .renderingMode(.original) // for images
+
     }
+               
+//   func executeDelete() {
+//       print("Now deleting...")
+//   }
 }
 
 struct ContentView_Previews: PreviewProvider {
