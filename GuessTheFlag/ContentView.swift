b1914eaaae4c2568accbfe9367a7c09a50612c16
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-10-23 18:11:58 +0300

    Day 20 — Using stacks to arrange views

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        VStack(spacing: 20) {
+//            Text("Hello, world!")
+//            Text("This is inside a stack")
+//        }
+        
+//        VStack(alignment: .leading) {
+//            Text("Hello, world!")
+//            Text("This is inside a stack")
+//        }
+        
+//        VStack {
+//            Text("First")
+//            Text("Second")
+//            Text("Third")
+//            Spacer()
+//        }
+        
+//        VStack {
+//            Spacer()
+//            Text("First")
+//            Text("Second")
+//            Text("Third")
+//            Spacer()
+//            Spacer()
+//        }
+        
+//        ZStack(alignment: .top) {
+//            Text("Hello, world!")
+//            Text("This is inside a stack")
+//        }
+        
+        VStack {
+            HStack {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            
+            HStack {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            
+            HStack {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
+        }
     }
 }
 
