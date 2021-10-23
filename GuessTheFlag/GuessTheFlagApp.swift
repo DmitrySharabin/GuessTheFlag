8f125418efee1f3c1bd6939f335dd1e54b0dcb23
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-10-23 18:04:39 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Dmitry Sharabin on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
